e75ec479dd781df786b26d3761eac5049b3fe0ad
Chris Manciero <cmanciero@gmail.com>
2026-04-28 15:04:03 -0400

    duh fix

--- src/context/authorization/index.js
+++ src/context/authorization/index.js
@@ -31,6 +31,14 @@ const getOAuthInfo = (countryISO) => {
       portalUrl: 'https://guinee.maps.arcgis.com/',
     });
   }
+
+  if(countryISO === 'PER'){
+    return new OAuthInfo({
+      appId: 'H0lqwISTHVGxKWeG',
+      popup: false,
+      portalUrl: 'https://mols.maps.arcgis.com/',
+    });
+  }
 };
 
 function AuthorizationProvider(props) {
--- src/hooks/useJWTToken.js
+++ src/hooks/useJWTToken.js
@@ -27,6 +27,14 @@ const getOAuthInfo = (countryISO) => {
       portalUrl: 'https://guinee.maps.arcgis.com/',
     });
   }
+
+  if(countryISO === 'PER'){
+    return new OAuthInfo({
+      appId: 'H0lqwISTHVGxKWeG',
+      popup: false,
+      portalUrl: 'https://mols.maps.arcgis.com/',
+    });
+  }
 };
 
 /**
