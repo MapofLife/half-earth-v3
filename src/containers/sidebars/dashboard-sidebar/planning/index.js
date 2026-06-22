7edc43610ed00b710bf67388f506f90bdaca91ef
Chris Manciero <cmanciero@gmail.com>
2026-06-22 11:23:37 -0400

    add fake priority layer

--- src/containers/sidebars/dashboard-sidebar/planning/index.js
+++ src/containers/sidebars/dashboard-sidebar/planning/index.js
@@ -1,37 +1,49 @@
-import React from 'react'
-import PlanningComponent from './planning-component'
+import React, { useRef } from 'react';
+import PlanningComponent from './planning-component';
 
 export const defaultPlanningOptions = [
-  { value: 0, label: 'Species', checked: false },
-  { value: 0, label: 'Ecosystem', checked: false },
-  { value: 0, label: 'Carbon', checked: false },
-  { value: 0, label: 'Water', checked: false },
-  { value: 0, label: 'Ecotourism', checked: false },
+  { id: 'species', value: 0, label: 'Species', checked: false },
+  { id: 'ecosystem', value: 0, label: 'Ecosystem', checked: false },
+  { id: 'carbon', value: 0, label: 'Carbon', checked: false },
+  { id: 'water', value: 0, label: 'Water', checked: false },
+  { id: 'ecotourism', value: 0, label: 'Ecotourism', checked: false },
 ];
 
 function PlanningContainer() {
-  const [planningOptions, setPlanningOptions] = React.useState(defaultPlanningOptions);
+  const ecoValue = useRef(0);
+  const speciesValue = useRef(0);
+  const [planningOptions, setPlanningOptions] = React.useState(
+    defaultPlanningOptions
+  );
   const [maximumArea, setMaximumArea] = React.useState(0);
 
   const displaySlider = (option, value, checked) => {
     // Update the checked status of the planning option
-    setPlanningOptions(prevOptions =>
-      prevOptions.map(opt =>
+    setPlanningOptions((prevOptions) =>
+      prevOptions.map((opt) =>
         opt.label === option.label ? { ...opt, checked } : opt
       )
     );
-  }
+  };
 
   const updateValue = (option, value) => {
     // Update the planning option value
     console.log(`Updating ${option.label} to value: ${value}`);
 
-    setPlanningOptions(prevOptions =>
-      prevOptions.map(opt =>
+    ecoValue.current = option.label === 'Ecotourism' ? value : ecoValue.current;
+    speciesValue.current =
+      option.label === 'Species' ? value : speciesValue.current;
+
+    setPlanningOptions((prevOptions) =>
+      prevOptions.map((opt) =>
         opt.label === option.label ? { ...opt, value } : opt
       )
     );
-  }
+
+    if (ecoValue.current >= 0.5 && speciesValue.current >= 0.5) {
+      console.log('Show layer');
+    }
+  };
 
   return (
     <PlanningComponent
@@ -41,7 +53,7 @@ function PlanningContainer() {
       maximumArea={maximumArea}
       setMaximumArea={setMaximumArea}
     />
-  )
+  );
 }
 
-export default PlanningContainer
+export default PlanningContainer;
